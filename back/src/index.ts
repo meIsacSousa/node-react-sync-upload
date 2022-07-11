b526aba80401ef16631e52d63633d06154b5b96e
Isac Sousa <isacsousadev@gmail.com>
2022-07-11 18:27:42 -0300

    feat: :sparkles: inclui estrutura inicial da aplicação

---
+++ back/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
