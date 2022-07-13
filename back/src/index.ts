dbdea71500940426ea17b59f1513d55aabcc7277
Isac Sousa <isacsousadev@gmail.com>
2022-07-12 21:10:20 -0300

    fix: :bug: Inclui tratamento para o não envio de arquivos

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { uploadRoutes } from './upload';
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const app = express();
 
--- back/src/upload.ts
+++ back/src/upload.ts
@@ -2,8 +2,14 @@ import { Response, Request, Router } from 'express';
 import upload from './config/uploadFile';
 
 const uploadRoutes = Router();
-uploadRoutes.post('/uploads', upload, (_req: Request, res: Response) => {
-    res.json({
+uploadRoutes.post('/uploads', upload, (req: Request, res: Response) => {
+    if (!req.file) {
+        return res.status(400).json({
+            message: 'No file provided'
+        });
+    }
+
+    return res.json({
         message: 'Uploaded successfully'
     });
 });
